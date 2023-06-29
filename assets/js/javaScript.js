843e6178d36615154eeeb88e9f797e75f7fb484d
Edsaugusto <edsonjota@hotmail.com>
2023-06-29 19:39:19 -0300

    adicionando o horario em js pra iniciar a contagem em 0 sec

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -1,4 +1,13 @@
 function main(){
+    function criaHora(segundos){
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-BR',{
+            hour12:false,
+            timeZone: 'GMT'
+        });
+    }
+    console.log(criaHora(10))
+
 
     const relogio = document.querySelector('.relogio');
     const iniciar = document.querySelector('.iniciar');
